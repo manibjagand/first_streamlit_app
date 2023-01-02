fb97f13c8d0a57c99d74db18c5893d6420cc73b5
manibjagand <121786007+manibjagand@users.noreply.github.com>
2023-01-02 16:03:20 +0530

    Update streamlit_app.py
    emoji changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,11 @@ st.title(' My parents New Healthy Diner')
 #streamlit.add('Menu Content')
 #st.write('Menu Content')
 st.header('Breakfast Menu')
-st.text(:bowl_with_spoon: 'omega 3 & Blueberry Oatmeal')
-st.text(:green_salad: 'Kale, Spinach & Rocket Smoothie')
-st.text(:chicken: 'Hard-Boiled Free-Range Egg')
-st.text(:avocado::bread: 'Avocado Toast')
+st.text(':bowl_with_spoon: omega 3 & Blueberry Oatmeal')
+st.text(':green_salad: Kale, Spinach & Rocket Smoothie')
+st.text(':chicken: Hard-Boiled Free-Range Egg')
+st.text(':avocado::bread: Avocado Toast')
+st.header(':banana::mango: Build Your Own Fruit Smoothie :kiwi_fruit::grapes:')
+
+    
+    
