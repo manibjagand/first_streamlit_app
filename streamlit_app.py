417c608581e204de62a4c23670a09ff23b32033d
manibjagand <121786007+manibjagand@users.noreply.github.com>
2023-01-02 16:39:29 +0530

    Update streamlit_app.py
    adding menu content by including url to code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title(' My parents New Healthy Diner')
 
@@ -11,5 +12,6 @@ st.text(':chicken: Hard-Boiled Free-Range Egg')
 st.text(':avocado::bread: Avocado Toast')
 st.header(':banana::mango: Build Your Own Fruit Smoothie :kiwi_fruit::grapes:')
 
-    
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
     
