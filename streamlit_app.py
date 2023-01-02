f18b11f234e34041dfe3fb3fc6265e581b5d51bc
manibjagand <121786007+manibjagand@users.noreply.github.com>
2023-01-02 16:55:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.text(':bowl_with_spoon: omega 3 & Blueberry Oatmeal')
 st.text(':green_salad: Kale, Spinach & Rocket Smoothie')
 st.text(':chicken: Hard-Boiled Free-Range Egg')
 st.text(':avocado::bread: Avocado Toast')
-st.header(':banana: :mango: Build Your Own Fruit Smoothie :kiwi_fruit: :grapes:')
+st.header(':banana:,:mango: Build Your Own Fruit Smoothie :kiwi_fruit:,:grapes:')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 st.dataframe(my_fruit_list)
