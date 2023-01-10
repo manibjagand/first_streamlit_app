cea695c8652fa057722ce229614d4b783f515091
manibjagand <121786007+manibjagand@users.noreply.github.com>
2023-01-10 18:09:14 +0530

    Update streamlit_app.py
    converting fruitvice data to jason()

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import requests
+
 
 st.title(' My parents New Healthy Diner')
 
@@ -21,6 +21,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 st.dataframe(fruits_to_show)
     
+streamlit.header('Fruityvice Fruit Advice!')
+import requests
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
