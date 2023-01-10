48be2e39f265c514f5c8f447d822f63616b7cc76
manibjagand <121786007+manibjagand@users.noreply.github.com>
2023-01-10 18:02:38 +0530

    Update streamlit_app.py
    import Requests with URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import requests
 
 st.title(' My parents New Healthy Diner')
 
@@ -20,3 +21,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 st.dataframe(fruits_to_show)
     
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
