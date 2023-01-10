5c56c161b0db0b697f9d36125e2aa03d32b95871
manibjagand <121786007+manibjagand@users.noreply.github.com>
2023-01-10 18:44:17 +0530

    Update streamlit_app.py
    rectifying errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,6 @@ fruityvice_response = r.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response.json())
 
 # Using Pandas Normalizing the Fruitvice 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # creating Streamlit Dataframe and Normalizing it.
-streamlit.dataframe(fruityvice_normalized)
+st.dataframe(fruityvice_normalized)
